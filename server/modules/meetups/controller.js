e93d2c683ad639f98d63b78e9259ad068c759b1d
Christopher Taylor <doctacloak@protonmail.com>
2018-09-13 10:58:25 -0500

    added api endpoint for getting all meetups

--- server/modules/meetups/controller.js
+++ server/modules/meetups/controller.js
@@ -10,3 +10,12 @@ export const createMeetup = async (req, res) => {
         return res.status(e.status).json({ error: true, message: 'Error with Meetup'})
     }
 }
+
+
+export const getAllMeetups = async (req, res) => {
+    try {
+        return res.status(200).json({ meetups: await Meetup.find( {} )})
+    } catch (e) {
+        return res.status(e.status).json({ error: true, message: 'Error with Meetup'})        
+    }
+}
