87360e994d9900d8c58e49e17c614fc0d686a913
Christopher Taylor <doctacloak@protonmail.com>
2018-09-13 15:43:48 -0500

    refactor: added parenthesis around the listener in server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use('/api', [MeetupRoutes]);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, err => {
+app.listen(PORT, (err) => {
     if (err) {
         console.log(err);
     } else {
