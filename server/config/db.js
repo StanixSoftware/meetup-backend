677da13c2963a67b078a5a6a568b71e61447a1d2
Christopher Taylor <doctacloak@protonmail.com>
2018-09-13 09:25:58 -0500

    added new url string parser to db config

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default () => {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/meetupApp');
+    mongoose.connect('mongodb://localhost/meetupApp', { useNewUrlParser: true });
     mongoose.connection
         .once('open', () => console.log('MongoDB Running.'))
         .on('error', err => console.log(err));
