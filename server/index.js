1c5b76c1ab80e03e03951eb597b5b41413591ffc
Christopher Taylor <doctacloak@protonmail.com>
2018-09-13 09:33:52 -0500

    added middlewares configuration

--- server/index.js
+++ server/index.js
@@ -2,15 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import dbConfig from './config/db';
+import middlewaresConfig from './config/middlewares';
 
 dotenv.config();
 
 const app = express();
 
 /*
-* Database
+*   Database
 */
 dbConfig();
+
+/*
+*   Middlewares
+*/
+middlewaresConfig(app);
 
 const PORT = process.env.PORT || 3000;
 
